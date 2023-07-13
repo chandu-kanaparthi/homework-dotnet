3673f558299a9165932ec1b75365f3be7a31fa20
chandu-kanaparthi <Chanduk6554@gmail.com>
2023-07-13 14:16:43 +0530

    homework

---
+++ SwEngHomework/SwEngHomework/Commissions/CommissionCalculator.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SwEngHomework.Commissions
+{
+    public class CommissionCalculator : ICommissionCalculator
+    {
+        private class InputData
+        {
+            public List<Advisor> Advisors { get; set; }
+            public List<Account> Accounts { get; set; }
+        }
+
+        private class Advisor
+        {
+            public string Name { get; set; }
+            public string Level { get; set; }
+        }
+
+        private class Account
+        {
+            public string Advisor { get; set; }
+            public double PresentValue { get; set; }
+        }
+
+        public IDictionary<string, double> CalculateCommissionsByAdvisor(string jsonInput)
+        {
+            var data = JsonConvert.DeserializeObject<InputData>(jsonInput);
+
+            var commissionRateByLevel = new Dictionary<string, double>
+            {
+                { "Senior", 1.0 },
+                { "Experienced", 0.5 },
+                { "Junior", 0.25 }
+            };
+
+            var basisPointsByValue = new (double limit, double bps)[]
+            {
+                (50000, 0.05),
+                (100000, 0.06),
+                (double.MaxValue, 0.07)
+            };
+
+            var commissionsByAdvisor = new Dictionary<string, double>();
+
+            foreach (var advisor in data.Advisors)
+            {
+                commissionsByAdvisor[advisor.Name] = 0;
+            }
+
+            foreach (var account in data.Accounts)
+            {
+                var advisorName = account.Advisor;
+                if (!commissionsByAdvisor.ContainsKey(advisorName)) continue;
+
+                double basisPoints = 0;
+
+                foreach (var (limit, bps) in basisPointsByValue)
+                {
+                    if (account.PresentValue < limit)
+                    {
+                        basisPoints = bps;
+                        break;
+                    }
+                }
+
+                var accountFee = account.PresentValue * basisPoints / 100;
+                var advisor = data.Advisors.Find(a => a.Name == advisorName);
+
+                commissionsByAdvisor[advisorName] += accountFee * commissionRateByLevel[advisor.Level];
+            }
+
+            foreach (var advisor in commissionsByAdvisor.Keys.ToList())
+            {
+                commissionsByAdvisor[advisor] = Math.Round(commissionsByAdvisor[advisor], 2);
+            }
+
+            return commissionsByAdvisor;
+        }
+    }
+}
---
+++ SwEngHomework/SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
@@ -0,0 +1,45 @@
+﻿namespace SwEngHomework.DescriptiveStatistics
+{
+    public class StatsCalculator : IStatsCalculator
+    {
+        public Stats Calculate(string semicolonDelimitedContributions)
+        {
+            var contributions = semicolonDelimitedContributions.Split(';');
+            List<double> contributionValues = new List<double>();
+
+            foreach (var contribution in contributions)
+            {
+                if (double.TryParse(contribution.Replace("$", "").Replace(",", ""), out double value))
+                {
+                    contributionValues.Add(value);
+                }
+            }
+
+            if (contributionValues.Count == 0)
+            {
+                return new Stats
+                {
+                    Average = 0,
+                    Median = 0,
+                    Range = 0
+                };
+            }
+
+            contributionValues.Sort();
+
+            double average = contributionValues.Average();
+            double median = contributionValues.Count % 2 == 0 
+                ? (contributionValues[contributionValues.Count / 2] + contributionValues[contributionValues.Count / 2 - 1]) / 2 
+                : contributionValues[contributionValues.Count / 2];
+
+            double range = contributionValues.Max() - contributionValues.Min();
+
+            return new Stats
+            {
+                Average = Math.Round(average, 2),
+                Median = Math.Round(median, 2),
+                Range = Math.Round(range, 2)
+            };
+        }
+    }
+}
